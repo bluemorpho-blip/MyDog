0c978ce7236fc16ac5ee5fc11b932a521a298609
Ronnie Stephens <53066783+bluemorpho-blip@users.noreply.github.com>
2020-02-25 01:06:15 -0500

    Update pet.rb

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -6,7 +6,7 @@ class Pet < ApplicationRecord
   validates :name, uniqueness: {scope: :user_id}
 
     account_sid = 'ACf6ddfec37ae5290c83a0f71e86a897d0'
-    auth_token = 'a77ad101c3df70a73b64782f7e775023'
+    auth_token = ''
     @client = Twilio::REST::Client.new(account_sid, auth_token)
   message = @client.messages
                   .create(
