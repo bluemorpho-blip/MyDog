454dcdbf82c56fa4db28d5e190d2ace0fe171881
Ronald Stephens <rstephens@bluemorpho.cc>
2020-03-01 22:04:10 -0500

    pushing MyDog Rails project to heroku as 'milk-and-honey'

--- db/migrate/20200216181442_create_users.rb
+++ db/migrate/20200216181442_create_users.rb
@@ -3,7 +3,7 @@ class CreateUsers < ActiveRecord::Migration[6.0]
     create_table :users do |t|
       t.string :name
       t.string :email
-      t.integer :phone_number
+      t.integer :phone_number, limit: 12
       t.string :contact_method
       t.string :password_digest
       t.boolean :admin, default: false
