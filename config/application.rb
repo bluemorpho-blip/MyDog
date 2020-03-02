769df5f80f49b9728d2bd604ae5059277e194765
Ronald Stephens <rstephens@bluemorpho.cc>
2020-03-01 19:25:00 -0500

    dotenv added to application.rb

--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,8 @@ require_relative 'boot'
 
 require 'rails/all'
 
+Dotenv::Railtie.load
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
