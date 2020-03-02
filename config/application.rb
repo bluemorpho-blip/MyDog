b29458dae7109c1f6b8aff075c44e842ade72fd1
Ronald Stephens <rstephens@bluemorpho.cc>
2020-03-01 19:44:59 -0500

    config/application.rb changed

--- config/application.rb
+++ config/application.rb
@@ -2,8 +2,6 @@ require_relative 'boot'
 
 require 'rails/all'
 
-Dotenv::Railtie.load
-
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
